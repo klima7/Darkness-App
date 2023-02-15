471bd84dcc11e6c8b7625d93f5de1ec0efe03d96
klima7 <ukasz.klimkiewicz@gmail.com>
2023-02-15 23:13:28 +0100

    Remove API app

--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import PairsList
-
-urlpatterns = [
-    path('pairs/', PairsList.as_view())
-]
--- darkness/settings.py
+++ darkness/settings.py
@@ -38,10 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'rest_framework',
     'core',
     'authentication',
-    'api',
     'flashcards',
 ]
 
