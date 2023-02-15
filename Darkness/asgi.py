0281bcae532dcead4733a830a3e3c4f1ca022544
klima7 <ukasz.klimkiewicz@gmail.com>
2023-02-15 15:43:05 +0100

    Rename

--- Darkness/asgi.py
+++ Darkness/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DarknessAPI project.
+ASGI config for Darkness project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DarknessAPI.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Darkness.settings')
 
 application = get_asgi_application()
--- Darkness/wsgi.py
+++ Darkness/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DarknessAPI project.
+WSGI config for Darkness project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DarknessAPI.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Darkness.settings')
 
 application = get_wsgi_application()
