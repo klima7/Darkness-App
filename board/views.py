655c891dc5f097af1a8affd0691608eba79692b4
klima7 <ukasz.klimkiewicz@gmail.com>
2023-02-16 16:17:04 +0100

    Fill board app

--- board/views.py
+++ board/views.py
@@ -5,4 +5,7 @@ from core.models import Pair
 
 @login_required
 def index(request):
-    return render(request, 'board/index.html')
+    pairs_raw = Pair.objects.all()
+    pairs_tmp = {(pair.first, pair.second): pair for pair in pairs_raw}
+    pairs = {first: {second: pairs_tmp[(first, second)] for second in Pair.LETTERS} for first in Pair.LETTERS}
+    return render(request, 'board/index.html', context={'pairs': pairs, 'letters': Pair.LETTERS})
