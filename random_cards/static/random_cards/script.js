d6ee247caa020ced8d78f62effdf312919f9cb36
klima7 <ukasz.klimkiewicz@gmail.com>
2023-02-16 22:00:48 +0100

    Add adjusting range in random cards app

--- random_cards/static/random_cards/script.js
+++ random_cards/static/random_cards/script.js
@@ -8,3 +8,24 @@ function revealPair(pair_id) {
         card.addClass('bg-light text-black revealed')
     }
 }
+
+function setSelects() {
+    console.log("Setting selects");
+    const urlParams = new URL(window.location.toLocaleString()).searchParams;
+
+    let fromLetter = urlParams.get('from') ?? 'a'
+    let toLetter = urlParams.get('to') ?? 'z'
+
+    $(`#select-from`).val(fromLetter).change()
+    $(`#select-to`).val(toLetter).change()
+}
+
+function generate() {
+    let letterFrom = $(`#select-from`).val()
+    let letterTo = $(`#select-to`).val()
+    const params = `?from=${letterFrom}&to=${letterTo}`;
+    const url = window.location.origin + window.location.pathname + params;
+    window.location = url;
+}
+
+setSelects()
--- random_cards/views.py
+++ random_cards/views.py
@@ -9,9 +9,10 @@ from core.models import Pair
 def index(request):
     from_letter = request.GET.get('from', 'a')
     to_letter = request.GET.get('to', 'z')
-    print(from_letter, to_letter)
-    pks = Pair.objects.values_list('id', flat=True)
-    pks = random.sample(list(pks), k=36)
-    pairs = list(Pair.objects.filter(id__in=pks).all())
-    random.shuffle(pairs)
-    return render(request, 'random_cards/index.html', {'pairs': pairs, 'letters': Pair.LETTERS.upper()})
+
+    matching_pairs = list(Pair.objects.filter(first__gte=from_letter, first__lte=to_letter).exclude(best__isnull=True).all())
+
+    count = min(36, len(matching_pairs))
+    random_pairs = random.sample(matching_pairs, k=count)
+    random.shuffle(random_pairs)
+    return render(request, 'random_cards/index.html', {'pairs': random_pairs, 'letters': Pair.LETTERS.upper()})
