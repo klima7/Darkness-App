24ee8099df767329d44909d173ca3bc3004ce84a
klima7 <ukasz.klimkiewicz@gmail.com>
2023-02-16 22:40:50 +0100

    Add dbl click to random cards

--- random_cards/static/random_cards/script.js
+++ random_cards/static/random_cards/script.js
@@ -1,31 +1,30 @@
 function revealPair(pair_id) {
     let card = $(`#pair-${pair_id}`);
-    if(card.hasClass('revealed')) {
-        window.open(`/admin/core/pair/${pair_id}/change/`, '_blank')
-    }
-    else {
-        card.removeClass('bg-dark text-white')
-        card.addClass('bg-light text-black revealed')
-    }
+    card.removeClass('bg-dark text-white');
+    card.addClass('bg-light text-black revealed');
+}
+
+function openPair(pair_id) {
+    window.open(`/admin/core/pair/${pair_id}/change/`, '_blank');
 }
 
 function setSelects() {
     console.log("Setting selects");
     const urlParams = new URL(window.location.toLocaleString()).searchParams;
 
-    let fromLetter = urlParams.get('from') ?? 'a'
-    let toLetter = urlParams.get('to') ?? 'z'
+    let fromLetter = urlParams.get('from') ?? 'a';
+    let toLetter = urlParams.get('to') ?? 'z';
 
-    $(`#select-from`).val(fromLetter).change()
-    $(`#select-to`).val(toLetter).change()
+    $(`#select-from`).val(fromLetter).change();
+    $(`#select-to`).val(toLetter).change();
 }
 
 function generate() {
-    let letterFrom = $(`#select-from`).val()
-    let letterTo = $(`#select-to`).val()
+    let letterFrom = $(`#select-from`).val();
+    let letterTo = $(`#select-to`).val();
     const params = `?from=${letterFrom}&to=${letterTo}`;
     const url = window.location.origin + window.location.pathname + params;
     window.location = url;
 }
 
-setSelects()
+setSelects();
