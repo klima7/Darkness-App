5d3e0f6007d2eb022be2ff985f29ea2ebea13969
klima7 <ukasz.klimkiewicz@gmail.com>
2023-02-16 21:19:46 +0100

    Add form to random cards page

--- random_cards/views.py
+++ random_cards/views.py
@@ -7,8 +7,11 @@ from core.models import Pair
 
 @login_required
 def index(request):
+    from_letter = request.GET.get('from', 'a')
+    to_letter = request.GET.get('to', 'z')
+    print(from_letter, to_letter)
     pks = Pair.objects.values_list('id', flat=True)
     pks = random.sample(list(pks), k=36)
     pairs = list(Pair.objects.filter(id__in=pks).all())
     random.shuffle(pairs)
-    return render(request, 'random_cards/index.html', {'pairs': pairs})
+    return render(request, 'random_cards/index.html', {'pairs': pairs, 'letters': Pair.LETTERS.upper()})
