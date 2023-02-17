b022350d7ed2f504c7c1b7ca97068e97e7605e0b
klima7 <ukasz.klimkiewicz@gmail.com>
2023-02-17 11:23:34 +0100

    Add basic tables

--- tables/views.py
+++ tables/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from core.models import Pair
+from core.models import Face
 
 
 def index(request):
-    return render(request, 'tables/index.html')
+    faces = Face.objects.all()
+
+    for face in faces:
+        pairs = [list(letter.pairs_first.all()) for letter in face.letters.all()]
+        face.zipped = list(zip(*pairs))
+        print(face.zipped)
+
+    return render(request, 'tables/index.html', context={'faces': faces})
