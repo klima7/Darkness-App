607580a8e0ec58fb459e4c70edbe1918e4be30f1
klima7 <ukasz.klimkiewicz@gmail.com>
2023-02-20 18:25:36 +0100

    Add selecting count to random cards

--- random_cards/static/random_cards/script.js
+++ random_cards/static/random_cards/script.js
@@ -10,15 +10,18 @@ function setSelects() {
 
     let fromLetter = urlParams.get('from') ?? 'a';
     let toLetter = urlParams.get('to') ?? 'z';
+    let count = urlParams.get('count') ?? '36';
 
     $(`#select-from`).val(fromLetter).change();
     $(`#select-to`).val(toLetter).change();
+    $(`#select-count`).val(count).change();
 }
 
 function generate() {
     let letterFrom = $(`#select-from`).val();
     let letterTo = $(`#select-to`).val();
-    const params = `?from=${letterFrom}&to=${letterTo}`;
+    let count = $(`#select-count`).val();
+    const params = `?from=${letterFrom}&to=${letterTo}&count=${count}`;
     const url = window.location.origin + window.location.pathname + params;
     window.location = url;
 }
--- random_cards/views.py
+++ random_cards/views.py
@@ -7,6 +7,7 @@ from core.models import Pair, Letter
 def index(request):
     from_char = request.GET.get('from', 'a')
     to_char = request.GET.get('to', 'z')
+    count = int(request.GET.get('count', '36'))
 
     from_letter = Letter.objects.filter(char=from_char).get()
     to_letter = Letter.objects.filter(char=to_char).get()
@@ -14,7 +15,12 @@ def index(request):
     letters = list(Letter.objects.filter(order__gte=from_letter.order, order__lte=to_letter.order).all())
     matching_pairs = list(Pair.objects.filter(first__in=letters).exclude(best__isnull=True).all())
 
-    count = min(36, len(matching_pairs))
+    count = min(count, len(matching_pairs))
     random_pairs = random.sample(matching_pairs, k=count)
     random.shuffle(random_pairs)
-    return render(request, 'random_cards/index.html', {'pairs': random_pairs, 'letters': Letter.objects.all()})
+    context = {
+        'pairs': random_pairs,
+        'letters': Letter.objects.all(),
+        'counts': [36, 72, 108, 144, 1000]
+    }
+    return render(request, 'random_cards/index.html', context)
