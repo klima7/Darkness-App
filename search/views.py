1a76f66d898ec6e081385a9b4807e761be9f04e7
klima7 <github@mail.ukasz.com>
2025-04-23 22:09:20 +0200

    Update search app

--- search/views.py
+++ search/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from core.models import Letter, Face
+
 
 def index(request):
-    return render(request, 'search/index.html', context={})
+    faces = Face.objects.all()
+    return render(request, 'search/index.html', context={'faces': faces})
