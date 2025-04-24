5679c5b86b8abac9c8a4642adca15c1c09468044
klima7 <github@mail.ukasz.com>
2025-04-24 22:18:13 +0200

    Optymize tables app

--- tables/views.py
+++ tables/views.py
@@ -3,11 +3,10 @@ from core.models import Face
 
 
 def index(request):
-    faces = Face.objects.all()
+    faces = Face.objects.all().prefetch_related('letters', 'letters__pairs_first')
 
     for face in faces:
         pairs = [list(letter.pairs_first.all()) for letter in face.letters.all()]
         face.zipped = list(zip(*pairs))
-        print(face.zipped)
 
     return render(request, 'tables/index.html', context={'faces': faces})
